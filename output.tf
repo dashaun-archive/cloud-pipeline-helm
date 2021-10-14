7d8964ac58ace59bf1164563b0f7a0938d8c6388
DaShaun <dashaun@redislabs.com>
2021-10-14 15:08:12 -0500

    remove local

--- output.tf
+++ output.tf
@@ -1,12 +1,3 @@
-locals {
-  redisgeek_config = {
-    kube_config_raw = azurerm_kubernetes_cluster.redisgeek.kube_config_raw
-    kube_config     = azurerm_kubernetes_cluster.redisgeek.kube_config
-  }
-  rg           = azurerm_resource_group.redisgeek.name
-  cluster_name = azurerm_kubernetes_cluster.redisgeek.name
-}
-
 output "resource_group" {
   value = module.aks-terraform-default.resource_group
 }
